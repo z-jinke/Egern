8a1c5d0dce4e2a59602cd20be2dab98b2ec4fda5
Z <155460219+z-jinke@users.noreply.github.com>
2025-09-06 15:04:45 +0800

    更新IpInfo.js

--- Js/IpInfo.js
+++ Js/IpInfo.js
@@ -1,3 +1,8 @@
+/**
+ * 作者: zjinke
+ * 功能: 查查询节点IP基本信息
+ */
+
 let url = "https://ipinfo.io/json?token=fa5efe047d9b4f";
 
 let countryMap = {
@@ -6,36 +11,42 @@ let countryMap = {
 };
 
 let fixedIcon = "location.north.circle.fill";
-let successColor = "#2293FF";
-let failColor = "#FF3B30";
+let iconColor = "#2293FF";
 
 $httpClient.get(url, (error, resp, body) => {
-    let ip = " ",
-        service = " ",
-        country = " ",
-        locationStr = " ",
-        iconColor = failColor;
+    let ipText = "";
+    let serviceText = "";
+    let country = "";
+    let latlngText = "";
 
     if (!error && body) {
         try {
             let json = JSON.parse(body);
-            ip = json.ip || ip;
-            if (json.org) {
-                let orgName = json.org.replace(/^AS\d+\s*/, "");
-                let parts = orgName.split(" ");
-                service = parts.slice(0, 2).join(" ");
-            }
+          
             if (json.country && json.country.length === 2) {
                 country = countryMap[json.country.toUpperCase()] || json.country.toUpperCase();
             }
-            locationStr = country;
-            iconColor = successColor;
+            if (json.ip) {
+                ipText = `IP地址：${json.ip}`;
+            }
+            if (json.org) {
+                let orgName = json.org.replace(/^AS\d+\s*/, "");
+                let parts = orgName.split(" ");
+                let serviceName = parts.slice(0, 2).join(" ");
+                serviceText = `运营商：${serviceName}`;
+            }
+            if (json.loc) {
+                let locParts = json.loc.split(",");
+                if (locParts.length === 2) {
+                    latlngText = `经纬度：${locParts[1]}-${locParts[0]}`;
+                }
+            }
         } catch (e) {}
     }
 
     $done({
-        title: `${locationStr}\n`,
-        content: `IP地址：${ip}\n运营商：${service}`,
+        title: country,
+        content: `${ipText}\n${serviceText}\n${latlngText}`,
         icon: fixedIcon,
         "icon-color": iconColor
     });
